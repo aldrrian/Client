2a4b18722194f5f212f770f6b70bac22a130d575
aldrrian <aldrrian@gmail.com>
2016-12-28 00:15:22 -0800

    changes in main - now is a server

---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/assign/list_of.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include "src/sockets/Udp.h"
+#include <unistd.h>
+
+using namespace std;
+using namespace boost::archive;
+std::stringstream ss;
+
+class Poind {
+    int x, y;
+
+    friend class boost::serialization::access;
+
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        ar & x;
+        ar & y;
+    }
+public:
+    Poind(int x, int y) : x(x), y(y) {}
+
+    Poind() : x(0), y(0) {}
+    int getX() {return this->x;}
+    int getY() {return this->y;}
+
+};
+
+class GridPoint {
+    Poind *p;
+
+    friend class boost::serialization::access;
+
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        ar & p;
+    }
+
+public:
+    GridPoint(Poind *p) : p(p) {};
+    GridPoint() : p(NULL) {}
+    virtual ~GridPoint() {
+        delete p;
+    }
+    friend std::ostream& operator<< (std::ostream &os, const GridPoint &p);
+};
+
+std::ostream& operator<< (std::ostream &os, const GridPoint &p) {
+    os << '(' << p.p->getX() << ',' << p.p->getY() << ')';
+    return os;
+}
+
+class D3GridPoint : public GridPoint {
+    int z;
+
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        ar & boost::serialization::base_object<GridPoint>(*this);
+        ar & z;
+    }
+public:
+    D3GridPoint(Poind *p, int z) : GridPoint(p), z(z) {};
+};
+
+
+
+int main(int argc, char *argv[]) {
+
+    GridPoint *gp = new GridPoint(new Poind(1,5));
+
+    std::string serial_str;
+    boost::iostreams::back_insert_device<std::string> inserter(serial_str);
+    boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(inserter);
+    boost::archive::binary_oarchive oa(s);
+    oa << gp;
+    s.flush();
+    cout << serial_str << endl;
+
+    cout << argv[1] << endl;
+    Udp udp(0, atoi(argv[1]));
+    udp.initialize();
+
+    char buffer[1024];
+    udp.sendData(serial_str);
+    udp.reciveData(buffer, sizeof(buffer));
+    cout << buffer << endl;
+
+
+    return 0;
+}
+
+
